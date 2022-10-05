e500ed5ced7efff4987a5082fe1038612047a6c3
Daniele <danileaodev@gmail.com>
2022-10-04 23:16:01 -0300

    Repo

--- src/modules/user/repositories/implementations/user-prisma.repository.ts
+++ src/modules/user/repositories/implementations/user-prisma.repository.ts
@@ -1,30 +1,24 @@
-import { User } from "../../../../entities/user.entity";
-import { prismaClient } from "../../../../shared/database/prisma/config/client";
-import { UserMap } from "../../mapper/user-map";
-import { IUserRepositoy } from "../user.repository";
+import { User } from '../../../../entities/user.entity'
+import { prismaClient } from '../../../../shared/database/prisma/config/client'
+import { UserMap } from '../../mapper/user-map'
+import { IUserRepositoy } from '../user.repository'
 
 export class UserPrismaRepo implements IUserRepositoy {
-    async findByUsername(username: string): Promise<User | null> {
-        console.log("ss")
+  async findByUsername(username: string): Promise<User | null> {
+    const user = await prismaClient.user.findFirst({
+      where: {
+        username,
+      },
+    })
 
-        const user = await prismaClient.user.findFirst({
-            where: {
-                username
-            }
-        })
+    if (!user) return null
 
-        if (!user) return null;
-
-        return UserMap.toDomain(user)
-
-
-
-    }
-    async save(data: User): Promise<User> {
-        const user = await prismaClient.user.create({
-            data
-        })
-        return user;
-    }
-
-} 
+    return UserMap.toDomain(user)
+  }
+  async save(data: User): Promise<User> {
+    const user = await prismaClient.user.create({
+      data,
+    })
+    return user
+  }
+}
